86ece10bd561c7bf2cd11a243aceffd86eaa9733
Eric Rasche <rasche.eric@yandex.ru>
2014-11-21 13:22:25 -0600

    Fixed redirect

--- registry/urls.py
+++ registry/urls.py
@@ -8,9 +8,8 @@ urlpatterns = patterns('',
     url(r'^u/(.*)$', views.reference),
     url(r'^create/$', views.add_phage),
     url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/phage-registry/'}),
     url(r'^about/', TemplateView.as_view(template_name="registry/about.html")),
     url(r'^rules/', TemplateView.as_view(template_name="registry/rules.html")),
     url(r'^list-data/$', views.OrderListJson.as_view(), name='order_list_json'),
 )
-
