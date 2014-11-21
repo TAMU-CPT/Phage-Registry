1cab04b3ce382ba9fb49dd844497386b4bef4901
Eric Rasche <rasche.eric@yandex.ru>
2014-11-21 13:57:23 -0600

    Added dbsource to admin

--- registry/admin.py
+++ registry/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import RegistryEntry
+from .models import RegistryEntry, DatabaseSource
 
 # Register your models here.
 admin.site.register(RegistryEntry)
+admin.site.register(DatabaseSource)
