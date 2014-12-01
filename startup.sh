a23b7f110b6a48d89ed1237422686eeb685ef942
Eric Rasche <rasche.eric@yandex.ru>
2014-12-01 14:50:40 -0600

    Only change admin password on boot, also wait on DB server to be ready

--- startup.sh
+++ startup.sh
@@ -4,14 +4,24 @@ export DOCKER_DB_USER=postgres
 export DOCKER_DB_HOST=$PHAGEREGDB_PORT_5432_TCP_ADDR
 export DJANGO_SETTINGS_MODULE=phageregistry.dockersettings
 
+# https://github.com/nathanleclaire/laraveldocker/commit/fb0f579f1482ba3396255b36c52506d71ef7fbcc#diff-76a1947cdef4e9ab08d8746a400e3681
+echo "Stalling for DB"
+while true;
+do
+    nc -q 1 $DOCKER_DB_HOST 5432 > /dev/null && break
+    sleep 1
+done
+
 mkdir -p /opt/static/
 python manage.py collectstatic --noinput -v 0
 
-PASS=$(date +%s | sha256sum | base64 | head -c 32)
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '$PASS')" | ./manage.py shell
-echo "Password is $PASS"
 # Sync the database models
 python manage.py syncdb --noinput
+
+PASS=$(date +%s | sha256sum | base64 | head -c 32)
+# This is kinda bad :(
+echo "from django.contrib.auth.models import User;user = User.objects.get(username='admin'); user.set_password('$PASS'); user.save()" | ./manage.py shell
+echo "Password is $PASS"
 
 # Reindex data (this needs to be a cronjob somehow, or data needs to be exposed in a volume)
 ./manage.py rebuild_index --noinput
