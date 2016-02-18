e54f9c3cec7c3df48e3a5d52d9f4b2fbc2131aed
Eric Rasche <rasche.eric@gmail.com>
2016-02-18 16:23:31 -0600

    Freeze reqs

--- Makefile
+++
@@ -1,2 +0,0 @@
-backup_db:
-	fig run backup /etc/cron.daily/export.sh
--- env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export DOCKER_DB_NAME=postgres
-export DOCKER_DB_USER=postgres
-export DOCKER_DB_HOST=$PHAGEREGDB_PORT_5432_TCP_ADDR
-export DJANGO_SETTINGS_MODULE=phageregistry.dockersettings
