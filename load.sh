234e1eea5f41ba7df7c3302ea42e3f0dada0da79
Eric Rasche <rasche.eric@gmail.com>
2016-08-03 21:46:47 +0000

    Remove python

--- load.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python manage.py db_load ncbi.json hxr NCBI --clean_name
-python manage.py db_load embl.json hxr EBI --clean_name
-python manage.py db_load phagesdb.json hxr mycophagedb
--- registry/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import RegistryEntry, DatabaseSource
-
-# Register your models here.
-admin.site.register(RegistryEntry)
-admin.site.register(DatabaseSource)
