fcbaa874d720872977fcb98b06fbf9413fa30dd9
Eric Rasche <rasche.eric@gmail.com>
2016-10-19 22:25:55 +0000

    Diff fuzziness

--- static/js/search.js
+++ static/js/search.js
@@ -25,8 +25,8 @@ function SearchCtrl($scope, $http, $routeParams, $log, $sce) {
             "query": {
                 "match": $scope.term,
                 "field": $scope.field,
-                //"prefix_length": 0,
-                "fuzziness": Math.floor($scope.term.length / 3),
+                "prefix_length": 2,
+                "fuzziness": $scope.term.length,
             }
         }).
         success(function(data) {
